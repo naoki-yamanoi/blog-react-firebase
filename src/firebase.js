f90087e2a010f0416cc7d7948c67923543fec049
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-05-12 17:48:07 +0900

    env作成

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAeH6fMUDR4trGNSXsuugJo9G_pa_lxfvY',
-  authDomain: 'blog-react-21984.firebaseapp.com',
-  projectId: 'blog-react-21984',
-  storageBucket: 'blog-react-21984.appspot.com',
-  messagingSenderId: '491755667994',
-  appId: '1:491755667994:web:9972c758a4c9fc5abe87a3',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "blog-react-21984.firebaseapp.com",
+  projectId: "blog-react-21984",
+  storageBucket: "blog-react-21984.appspot.com",
+  messagingSenderId: "491755667994",
+  appId: "1:491755667994:web:9972c758a4c9fc5abe87a3",
 };
 
 // Initialize Firebase
