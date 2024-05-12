bc563a285d3cdfe794838ccae396287b66f43f7f
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-05-12 16:19:26 +0900

    ログイン実装

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import CreatePost from "./components/CreatePost";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
     <Router>
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/createpost" element={<CreatePost></CreatePost>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
+        <Route
+          path="/login"
+          element={<Login setIsAuth={setIsAuth}></Login>}
+        ></Route>
         <Route path="/logout" element={<Logout></Logout>}></Route>
       </Routes>
     </Router>
