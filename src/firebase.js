a078ee289ffb07c29a085de97da103a24ca4843b
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-05-12 11:41:52 +0900

    firebaseとreactを連携

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAeH6fMUDR4trGNSXsuugJo9G_pa_lxfvY',
+  authDomain: 'blog-react-21984.firebaseapp.com',
+  projectId: 'blog-react-21984',
+  storageBucket: 'blog-react-21984.appspot.com',
+  messagingSenderId: '491755667994',
+  appId: '1:491755667994:web:9972c758a4c9fc5abe87a3',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
